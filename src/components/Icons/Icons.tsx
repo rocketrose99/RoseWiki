7633c17800a9c4446fc029f10ec26dc6d31040a4
S. Rose Lindauer <sarah.lindauer@gmail.com>
2026-01-05 21:53:06 -0700

    feat: add tooltips to github and linked in icons

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,28 +1,32 @@
 import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react';
-import { ActionIcon, Group } from '@mantine/core';
+import { ActionIcon, Group, Tooltip } from '@mantine/core';
 import { ColorSchemeToggle } from '../ColorSchemeToggle/ColorSchemeToggle';
 
 export function Icons() {
   return (
     <Group justify="flex-end" gap="xs">
-      <ActionIcon
-        component="a"
-        href="https://github.com/rocketrose99?tab=repositories"
-        size="md"
-        aria-label="Open in a new tab"
-        variant="light"
-      >
-        <IconBrandGithub />
-      </ActionIcon>
-      <ActionIcon
-        component="a"
-        href="https://www.linkedin.com/in/s-rose-lindauer/"
-        size="md"
-        aria-label="Open in a new tab"
-        variant="light"
-      >
-        <IconBrandLinkedin />
-      </ActionIcon>
+      <Tooltip label="See my GitHub repositories">
+        <ActionIcon
+          component="a"
+          href="https://github.com/rocketrose99?tab=repositories"
+          size="md"
+          aria-label="Open in a new tab"
+          variant="light"
+        >
+          <IconBrandGithub />
+        </ActionIcon>
+      </Tooltip>
+      <Tooltip label="See my LinkedIn profile">
+        <ActionIcon
+          component="a"
+          href="https://www.linkedin.com/in/s-rose-lindauer/"
+          size="md"
+          aria-label="Open in a new tab"
+          variant="light"
+        >
+          <IconBrandLinkedin />
+        </ActionIcon>
+      </Tooltip>
       <ColorSchemeToggle />
     </Group>
   );
